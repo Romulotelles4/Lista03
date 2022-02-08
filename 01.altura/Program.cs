27823e6c9283b4fcc51093b18ad0e99ba1a5db30
Romulo Carneiro <romulocarneiro2002@hotmail.com>
2022-02-08 17:06:13 -0300

    Add project files.

---
+++ 01.altura/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _01.altura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior = 0, menor, i = 0;
+
+            Console.WriteLine("Informe o 1° número");
+            int altura = Convert.ToInt32(Console.ReadLine());
+            maior = altura;
+            menor = altura;
+            i = 1;
+
+            for (i=2; i<=15; i++)
+            {
+                Console.WriteLine("Informe o " + i + "° número:");
+                altura = Convert.ToInt32(Console.ReadLine());      
+                
+                
+                if(altura < menor)
+                {
+                    menor = altura;
+                }
+
+                if (altura > maior)
+                {
+                    maior = altura;
+                }
+
+            }
+            Console.WriteLine("A menor altura é: " + menor);
+            Console.WriteLine("A maior altura é: " + maior);
+            Console.ReadLine();
+
+
+        }
+    }
+}
